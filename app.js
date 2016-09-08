31aeac5a978a44e95fd2e1531f415d4b8f2960ed
Yeshe Wingerd <yeshito@gmail.com>
2016-09-07 17:36:12 -0700

    got comment adding to work

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const app = angular.module('redditClone', []);
+const app = angular.module('redditClone', ['ngRoute']);
 
-app.router('/',)
+app.config(function($routeProvider) {
+  $routeProvider.
+  when('/', {
+    templateUrl: 'index.html',
+    controller: 'RedditController'
+  })
+})
 
 app.controller('RedditController', function ($scope) {
   $scope.reddit = {};
-  $scope.reddit.posts = [];
   $scope.reddit.showpostForm = false;
+  $scope.reddit.posts = [];
 
   $scope.reddit.Post = function () {
     this.title = $scope.reddit.title;
@@ -16,6 +22,14 @@ app.controller('RedditController', function ($scope) {
     this.description = $scope.reddit.description;
     this.date = new Date();
     this.comments = [];
+    this.addComment = function () {
+      this.comments.push({author: this.commentAuthor , comment: this.comment})
+    }
+    this.clearCommentForm = function () {
+      this.commentAuthor = '';
+      this.comment = '';
+    }
+
     this.votes = 0;
     this.voteUp = function () {
       this.votes++;
@@ -23,64 +37,27 @@ app.controller('RedditController', function ($scope) {
     this.voteDown = function () {
       this.votes--;
     }
+
   };
 
   $scope.reddit.addPost = function () {
-   $scope.reddit.posts.push(new $scope.reddit.Post());
-   console.log(JSON.stringify($scope.reddit.posts));
+    $scope.reddit.posts.push(new $scope.reddit.Post());
   }
+  $scope.reddit.postFormClear = function() {
+    $scope.reddit.title='';
+    $scope.reddit.author='';
+    $scope.reddit.imageURL='';
+    $scope.reddit.description='';
+  }
+
 })
 
 app.directive('postCard', function () {
     return {
-        template: templateString
+        templateUrl: './partials/postCard.html',
+        restrict: 'E',
+        scope: {
+          postCard: '='
+        }
     };
 })
-
-const templateString = `<div class="row">
-  <div class="col l12">
-    <div class="card horizontal hoverable">
-      <div class="card-image">
-        <img src="{{post.imageURL}}">
-      </div>
-      <div class="card-stacked">
-        <div class="card-content">
-          <div class="row">
-            <div class="col l8">
-              <h5>{{post.title}}</h5>
-              <h6>by {{post.author}}</h6>
-            </div>
-            <div class="col l3">
-              <a ng-click="post.voteUp()" href=""><i class="small material-icons">thumb_up</i></a>
-              <a ng-click="post.voteDown()" href=""><i class="small material-icons">thumb_down</i></a>
-            </div>
-            <div class="col l1">
-              <h5 ng-class="{'color-green': post.votes > 0, 'color-red': post.votes < 0}">{{post.votes}}</h5>
-            </div>
-          </div>
-          <p>{{post.description}}</p>
-        </div>
-        <div class="card-action">
-          <div class="row">
-            <div class="col l6">
-              <p>{{post.date}}</p>
-            </div>
-            <div class="col l3">
-              <a ng-click="post.votes += 1" class="activator" href=""><i class="material-icons">chat_bubble_outline</i>1 comment</a>
-            </div>
-            <div class="col l3">
-              <a ng-click="post.votes -= 1" class="activator" href=""><i class="material-icons">mode_edit</i>Add Comment</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card-reveal">
-        <div class="row showComments">
-          <span class="card-title grey-text text-darken-4">Comment<i class="material-icons right">close</i></span>
-          <p>Johnny: Why is this guy so amazing!?</p>
-          <p>Samantha: Well he works the shit out of shit, and has devoted himself fully. What an expressive player...</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`
